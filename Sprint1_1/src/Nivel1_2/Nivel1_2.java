10a25ac8a88297d72b097285de550073695e3f25
jaime-hugo <huamanchumopacheco@gmail.com>
2024-08-08 21:50:35 +0200

    Cambio

--- Sprint1_1/src/Nivel1_2/Nivel1_2.java
+++ Sprint1_1/src/Nivel1_2/Nivel1_2.java
@@ -1,7 +1,16 @@
 package Nivel1_2;
 
+import static Nivel1_2.Coche.frenar;
+
 public class Nivel1_2 {
     public static void main(String[] args) {
+
+        Coche coche = new Coche ("Algo");
+
+        Coche.modelo = "CP4";
+        frenar();
+        coche.acelerar();
+        System.out.println(coche.toString());
 
     }
 }
